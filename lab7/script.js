b01f0355ca0144fe866b2cbf16efcea7a52dce97
Manaquib <95608256+Manaquib@users.noreply.github.com>
2022-02-07 09:32:32 +0530

    commit

--- lab7/script.js
+++ lab7/script.js
@@ -6,7 +6,13 @@ function printf2() {
     document.write("Hello World.")
 }
 
+function add() {
+    var a=10, b=12;
+    document.write("Addition of a and b is = "+(a+b))
+}
+
 document.getElementById("submit").addEventListener("click", ()=>{
     printf()
     printf2()
+    add()
 })
